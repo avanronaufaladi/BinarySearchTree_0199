a3805ba53a2d1ac1918419aa490fa44ce2db6b07
avanronaufaladi <avaroadi@gmail.com>
2024-06-13 13:18:17 +0700

    memasukkan lokasi memori node baru

--- BinarySearchTree_0199.cpp
+++ BinarySearchTree_0199.cpp
@@ -27,4 +27,7 @@ public:
 	{
 		root = nullptr; //initializing root to null
 	}
-	
+	void insert(string element) //insert a node in the binary search tree
+	{
+		Node* newNode = new Node(element, nullptr, nullptr); //allocate memory for the new node
+		
