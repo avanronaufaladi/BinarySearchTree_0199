28f81b85e82fab4d15cf6b7965175cd7e2c9aed0
avanronaufaladi <avaroadi@gmail.com>
2024-06-13 13:21:08 +0700

    lokasi node yg disimpan

--- BinarySearchTree_0199.cpp
+++ BinarySearchTree_0199.cpp
@@ -33,3 +33,9 @@ public:
 		newNode->info = element; //assign value to the data field of the new node
 		newNode->leftchild = nullptr; //make the left child of the new node point to null
 		newNode->rightchild = nullptr; //make the right child of the new node point to null
+
+		Node* parent = nullptr;
+		Node* currentnode = nullptr;
+		search(element, parent, currentnode); //locate the node which will be the parent of the node to be insert
+
+		
