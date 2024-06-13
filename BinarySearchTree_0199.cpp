d9403446b5c92bdf17bacafd447092ae235211a5
avanronaufaladi <avaroadi@gmail.com>
2024-06-13 13:20:21 +0700

    memasukkan point node baru kedalam data

--- BinarySearchTree_0199.cpp
+++ BinarySearchTree_0199.cpp
@@ -30,4 +30,6 @@ public:
 	void insert(string element) //insert a node in the binary search tree
 	{
 		Node* newNode = new Node(element, nullptr, nullptr); //allocate memory for the new node
-		
+		newNode->info = element; //assign value to the data field of the new node
+		newNode->leftchild = nullptr; //make the left child of the new node point to null
+		newNode->rightchild = nullptr; //make the right child of the new node point to null
